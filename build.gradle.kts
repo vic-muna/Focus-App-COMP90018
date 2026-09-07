d44c933069d78c696064fc3d9c247d1301bef44c
nicklu356 <nicklu356@gmail.com>
2026-09-07 12:02:51 +1000

    updating therefore Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,8 +0,0 @@
-// Top-level build file. Declares which versions of the Android Gradle
-// Plugin (AGP) and Kotlin the project uses, but does not apply them here
-// (apply false) - the ":app" module applies them itself.
-plugins {
-    id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
-}
